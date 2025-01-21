5b149e70c6663e318f2e5047576901a2a9ee4154
Lucio Baiocchi <lucio.baiocchi@studio.unibo.it>
2025-01-21 18:31:41 +0100

    added java part

---
+++ control-unit-backend/gradle/wrapper/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("java")
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("io.github.java-native:jssc:2.9.6")
+    implementation("org.slf4j:slf4j-simple:2.0.9")
+    implementation ("org.jfree:jfreechart:1.5.3")
+
+}
+
+application {
+    mainClass.set("main")
+}
+
+
+tasks.test {
+    useJUnitPlatform()
+}
