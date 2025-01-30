0238e20be0d6283703d811c45043d4707c0a0da6
Filippo Massari <filippo.massari6@studio.unibo.it>
2025-01-30 12:18:37 +0100

    try to fix commit problem

--- temperature-monitoring-subsystem-esp/src/main.cpp
+++ temperature-monitoring-subsystem-esp/src/main.cpp
@@ -1,67 +1,53 @@
-/*
- * HTTPClient lib --  Performing an HTTP POST to our REST service
- *
- * Remark:
- * - Going through ngrok
- *
- */
-#include <WiFi.h>
-#include <HTTPClient.h>
+#include <Arduino.h>
+#include "./data/api/SharedState.h"
+#include "./task/api/LedTask.h"
+#include "./task/api/TemperatureTask.h"
+#include "./task/api/MqttTask.h"
 
-const char* ssid = "lucio";
-const char* password = "lupolucio";
+// Oggetto SharedState condiviso
+SharedState sharedState;
 
-const char *serviceURI = "http://172.20.10.13:8080";
+// TaskHandle_t per le task
+TaskHandle_t LedTaskHandle;
+TaskHandle_t TemperatureTaskHandle;
+TaskHandle_t MqttTaskHandle;
 
+// Funzioni delle task
+void LedTaskFunction(void* parameter);
+void TemperatureTaskFunction(void* parameter);
+void MqttTaskFunction(void* parameter);
 
-void connectToWifi(const char* ssid, const char* password){
-  WiFi.begin(ssid, password);
-  Serial.println("Connecting");
-  while(WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected to WiFi network with IP Address: ");
-  Serial.println(WiFi.localIP());
+void LedTaskFunction(void* parameter) {
+    LedTask ledTask(LED_GREEN_PIN, LED_RED_PIN, sharedState);
+    for (;;) {
+        ledTask.update();
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+    }
+}
+
+void TemperatureTaskFunction(void* parameter) {
+    TemperatureTask temperatureTask(TEMP_SENSOR_PIN, sharedState);
+    for (;;) {
+        temperatureTask.update();
+        vTaskDelay(sharedState.getFrequency() / portTICK_PERIOD_MS);
+    }
+}
+
+void MqttTaskFunction(void* parameter) {
+    MqttTask mqttTask(MQTT_SERVER, MQTT_PORT, MQTT_CLIENT_ID, sharedState);
+    for (;;) {
+        mqttTask.update();
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+    }
 }
 
 void setup() {
-  Serial.begin(115200); 
-  connectToWifi(ssid, password);
-}
+    Serial.begin(9600);
 
-int sendData(String address, float value, String place){  
-  
-   HTTPClient http;    
-   http.begin(address + "/api/data");      
-   http.addHeader("Content-Type", "application/json");    
-    
-   String msg = 
-    String("{ \"value\": ") + String(value) + 
-    ", \"place\": \"" + place +"\" }";
-   
-   int retCode = http.POST(msg);   
-   http.end();  
-      
-   return retCode;
+    xTaskCreatePinnedToCore(LedTaskFunction, "LedTask", 10000, NULL, 1, &LedTaskHandle, 1); 
+    xTaskCreatePinnedToCore(TemperatureTaskFunction, "TemperatureTask", 10000, NULL, 1, &TemperatureTaskHandle, 1);
+    xTaskCreatePinnedToCore(MqttTaskFunction, "MqttTask", 10000, NULL, 1, &MqttTaskHandle, 0);
 }
 
 void loop() {
-  if (WiFi.status()== WL_CONNECTED){      
-
-    int value = random(15,20);
-    int code = sendData(serviceURI, value, "lab_2_2");
-    if (code == 200){
-       Serial.println("ok");   
-     } else {
-       Serial.println(String("error: ") + code);
-     }
-    
-    delay(5000);
-
-  } else {
-    Serial.println("WiFi Disconnected... Reconnect.");
-    connectToWifi(ssid, password);
-  }
 }
